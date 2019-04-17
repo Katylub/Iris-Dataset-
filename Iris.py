d59acb2a0639785420a66b0ea2eea790fa447cd2
Katylub <Katy@gmit.ie>
2019-04-17 19:41:58 +0100

    Review 4

--- Iris.py
+++ Iris.py
@@ -6,6 +6,7 @@
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 #Read the Data
 f= pd.read_csv('IrisDataset.csv')
@@ -18,3 +19,24 @@ print("The shape of dataset is:", (f.shape))
 
 # Give description of dataset
 print (f.describe())
+
+#Check class
+print(f.groupby('species').size())
+
+#Univariable plot
+
+# box and whisker plots
+f.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+plt.show()
+
+# histograms
+f.hist()
+plt.show()
+
+#Boxplot
+f.boxplot(by='species')
+plt.show()
+
+#Scatter
+pd.plotting.scatter_matrix(f)
+plt.show()
